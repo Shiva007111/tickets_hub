e21ea06d1f0e831a60009c87d53e125acedf94cb
Shiva Kumar <shivakumar@saranyu.in>
2025-06-19 17:14:27 +0530

    resolved issue database connection

--- config/database.js
+++ config/database.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 const { Client } = require("pg");
-console.log("DB URL from env from database js:", process.env.DATABASE_URL);
  
 const client = new Client({
-	/*
-	 *   host: "etvwin-userdb.cwswgtycunqq.ap-south-1.rds.amazonaws.com",
-	 *     port: 4615,
-	 *       user: "postgres",
-	 *         password: "be562d3796379b9801db508947d67388",
-	 *           database: "ott_services_db",
-	 *           */
-	connectionString: process.env.DATABASE_URL,
-	 /* ssl: {
-		      rejectUnauthorized: false, // Optional: Useful if you're using SSL without a verified certificate
-		    },*/
+  /*
+   *   host: "etvwin-userdb.cwswgtycunqq.ap-south-1.rds.amazonaws.com",
+   *     port: 4615,
+   *       user: "postgres",
+   *         password: "be562d3796379b9801db508947d67388",
+   *           database: "ott_services_db",
+   *           */
+  connectionString: process.env.DATABASE_URL,
+   /* ssl: {
+          rejectUnauthorized: false, // Optional: Useful if you're using SSL without a verified certificate
+        },*/
 });
 client
   .connect()
