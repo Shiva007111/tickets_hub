0fed384426256e3c8fd57d425118ad654ae48c65
Shiva Kumar <shivakumar@saranyu.in>
2025-06-20 17:26:42 +0530

    Issue resolved

--- routes/routes.js
+++ routes/routes.js
@@ -17,11 +17,11 @@ router.post('/platforms/genres/create', platfromController.genreCreate);
 router.get('/platforms/genres/all', platfromController.genreAll);
 router.delete('/platforms/genres/:id', platfromController.genreDelete);
 
-router.post('/paltforms/locations/create', platfromController.locationCreate);
+router.post('/platforms/locations/create', platfromController.locationCreate);
 router.get('/platforms/locations/all', platfromController.locationAll);
 router.delete('/platforms/locations/:id', platfromController.locationDelete);
 
-router.post('/paltforms/languages/create', platfromController.languageCreate);
+router.post('/platforms/languages/create', platfromController.languageCreate);
 router.get('/platforms/languages/all', platfromController.languageAll);
 router.delete('/platforms/languages/:id', platfromController.locationDelete);
 
